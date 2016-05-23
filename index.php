6c16900149c1f7d880c9a84b772b6b3eeff4fc39
davidswinburne <davidswinburne@users.noreply.github.com>
2016-05-23 12:47:18 +1000

    updated index for user login fields
    updated index for user login fields

--- index.php
+++ index.php
@@ -39,7 +39,29 @@
      <h1><div align="center"><img src="images/pharmacy.jpg" alt="Pharmacy" title="Pharmacy" width="450" height="180 " "font-family:verdana"/><br/>People Health Pharmacy</h1></div>
 	 </div> 
       <div align = "center">
-	  
+	  <div style="max-width:1000px;
+	    margin-top:10px;
+	    margin-left: auto;
+	    margin-right: auto;
+	    margin-bottom:50px;
+	    background-color: #F8F8FF;
+	    border: 6px outset #FF0000;
+	    border-radius: 15px;
+        text-align:center;">
+            <div style = "background-color:#81F7F3; color:#FFFFFF; padding:8px;"><b>PHP SRS User Login </b></div>
+               <form action = "" method = "post">
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/>
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = "Login"/><br />
+               </form></div>
+               <?php  
+               if($_SERVER["REQUEST_METHOD"] == "POST") {
+               login($_POST);
+           }
+               	?>
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px">
+               <?php echo $error; ?>
+               </div>
 					
             </div>
 				
